91ada94342ea75ffc68f0304ccb87fefe879ce75
Ben10Formis <ben10formis@mail.com>
2017-06-19 01:21:41 -0300

    http://academia-binaria.com/enrutado-con-angular2-el-nuevo-spa/ Erutado <router-outlet></router-outlet> Menu

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,18 +1,41 @@
+// importación de otros módulos de funcionalidad
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+//
+/*import { AlertModule } from 'ng2-bootstrap';
+import { AuthGuard } from './_guards/index';
+import { AuthenticationService, UserService, ProductService  } from './_services/index';*/
 
+// importación de módulo de enrutado asociado
+import { routing } from './app.routing';
+/*import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/index';*/
+import { HomeComponent } from './components/home/index';
+/*import { NoContentComponent } from './no-content/index';
+import { LoginComponent } from './login/index';
+
+import { ProductsComponent, DetailProductsComponent, NewProductsComponent, ListProductsComponent } from './home/products/index';
+
+import { OrdersComponent } from './home/orders/orders.component';
+
+import { ClientComponent, NewClientComponent, DetailClientComponent } from './home/client/index';
+import { FileUploadModule } from 'ng2-file-upload/ng2-file-upload';*/
+//
+// decorador que define un módulo
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent, // el módulo funcional para la 'página home'
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing,       // el módulo de rutas ya configurado
   ],
   providers: [],
   bootstrap: [AppComponent]
